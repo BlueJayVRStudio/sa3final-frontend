90a25ffbd8526ac153a7a3791882bd030d335ef1
BlueJayVRStudio <111257752+BlueJayVRStudio@users.noreply.github.com>
2023-10-08 01:57:41 -0500

    update 10/08 1:57 am
    update 10/08 1:57 am

--- app.py
+++ app.py
@@ -12,28 +12,44 @@ dataanalyzer_addr = "http://34.118.238.31:5051/"
 
 @app.route("/")
 def main():
-    return render_template("table.html")
-
-@app.route("/retrieve_records", methods=["GET"])
-def retrieve_records():
-    response = requests.get(dataanalyzer_addr + "retrieve_records")
-    return response.text
+    return render_template("table.html", message=None, failed=False)
 
 @app.route("/scrape", methods=["GET"])
 def call_scrape():
-    response = requests.get(dataanalyzer_addr + "scrape")
-    return response.text
+    try:
+        response = requests.get(dataanalyzer_addr + "scrape")
+    except:
+        return render_template("table.html", message="Data collector component failed to scrape data :(", failed=True)
+    
+    if response.status_code == 200:
+        return render_template("table.html", message="Data successfully scraped by the data collector component!!", failed=False)
+    else:
+        return render_template("table.html", message="Data collector component failed to scrape data :(", failed=True)
 
 @app.route("/delete_records", methods=["GET"])
 def call_delete():
-    response = requests.get(dataanalyzer_addr + "delete_records")
-    return response.text
+    try:
+        response = requests.get(dataanalyzer_addr + "delete_records")
+    except:
+        return render_template("table.html", message="The data collector component failed to delete data :(", failed=True)
+    
+    if response.status_code == 200:
+        return render_template("table.html", message="All records successfully deleted by the data collector component!!", failed=False)
+    else:
+        return render_template("table.html", message="The data collector component failed to delete data :(", failed=True)
 
 @app.route("/table")
 def table():
-    response = requests.get(dataanalyzer_addr + "retrieve_records")
+    try:
+        response = requests.get(dataanalyzer_addr + "retrieve_records")
+    except:
+        return render_template("table.html", message="Failed to retrieve table :(", failed=True)
+    
     rows = json.loads(response.text)
-    return render_template("table.html", rows=rows)
+    if response.status_code == 200:
+        return render_template("table.html", rows=rows, message="Table retrieved!", failed=False)
+    else:
+        return render_template("table.html", rows=rows, message="Failed to retrieve table :(", failed=False)
 
 
 if __name__ == "__main__":
